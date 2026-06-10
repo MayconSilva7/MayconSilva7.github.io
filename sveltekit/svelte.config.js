cf0cf39006b51f02e6552a6898a90f009f573c58
MaithGuy7 <122628753+MayconSilva7@users.noreply.github.com>
2026-06-10 19:03:37 +0200

    Update svelte.config.js

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -3,23 +3,23 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/cv-website'
-		},
-		prerender: {
-			entries: ['*']
-		}
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: ''
+        },
+        prerender: {
+            entries: ['*']
+        }
+    }
 };
 
 export default config;
